896506983eed1fcc43916f0d624b0c97a011a6f0
frisaac <frisaac@stu.naperville203.org>
2018-08-10 16:43:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 # dog.rb
 class Dog
-  def name = (name)
-    @name = name
+  def name=(name)
+    @name=name
   end
   def name
     @name
   end
-  def breed = (breed)
-    @breed = breed
+  def breed=(breed)
+    @breed=breed
   end
   def breed
     @breed
